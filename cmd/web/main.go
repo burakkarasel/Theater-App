aeef652a2b08c3ba7e1584833061b3dfedc6013a
kullop <burakcankarasel@gmail.com>
2022-08-19 12:21:44 +0300

    Added server, port, db to main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/burakkarasel/Theatre-API/internal/api"
+	db "github.com/burakkarasel/Theatre-API/internal/db/sqlc"
 	"github.com/burakkarasel/Theatre-API/internal/dsn"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = dsn.DSN
+	port     = "localhost:8080"
 )
 
 func main() {
+	// first i connect to DB
+	conn, err := sql.Open(dbDriver, dbSource)
 
+	if err != nil {
+		log.Fatal("cannot connect to DB:", err)
+	}
+
+	log.Println("connected to DB")
+
+	// then i run my migrations
 	runDBMigration("file://internal/db/migration", dsn.DSN)
+
+	// then i create a new store to create a new server
+	store := db.NewStore(conn)
+
+	log.Println("created a new store instance")
+
+	// then i create a new server to run the server
+	server := api.NewServer(store)
+
+	log.Println("created a new server instance")
+
+	err = server.Start(port)
+
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
 
 // runDBMigration runs the migrations at the start of the program
