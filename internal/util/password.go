4366db9c89f01ba8f16669da6ee939aee72192e3
kullop <burakcankarasel@gmail.com>
2022-08-19 12:23:25 +0300

    Added hash and compare password functions

---
+++ internal/util/password.go
@@ -0,0 +1,14 @@
+package util
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword takes a password string as input an returns the hashed password and possible error
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
+
+// CompareHashedPassword takes password and hashed password as input and returns a possible error
+func CompareHashedPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
